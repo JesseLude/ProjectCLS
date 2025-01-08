9432af22b46941fb3b82f43a7ac3bfbfc0e03504
dionspion <dionh@kpnmail.nl>
2025-01-08 17:47:28 +0100

    Update DLA.py

--- DLA.py
+++ DLA.py
@@ -7,22 +7,25 @@ grid_size = 200
 grid = np.zeros((grid_size, grid_size))
 grid[grid_size // 2, grid_size // 2] = 1          # set a seed in the middle
 
-nutrient_map = np.linspace(1, 0.1, grid_size).reshape(-1, 1)
-
-def random_walk():
+nutrient_map = np.outer(np.linspace(1, 0.1, grid_size),np.linspace(1, 0.5, grid_size))
+def random_walk(max_steps = 1000):
     x, y = np.random.randint(0, grid_size, size=2)
-    while True:
+    steps = 0
+
+    while steps < max_steps:
         x += np.random.choice([-1, 0, 1])
         y += np.random.choice([-1, 0, 1])
         x, y = np.clip(x, 1, grid_size-2), np.clip(y, 1, grid_size-2)
+
         if grid[x-1:x+2, y-1:y+2].any():
-            print(nutrient_map[x, y])
-            if np.random.rand() < nutrient_map[x, y]:  # Nutrient-based probability
+            if np.random.rand() < nutrient_map[x, y]:  # nutrient-based probability
                 grid[x, y] = 1
             break
 
+        steps += 1
+
 # simulate growth
-for i in range(5000):
+for i in range(500):
     random_walk()
 
 plt.imshow(grid, cmap='viridis')
